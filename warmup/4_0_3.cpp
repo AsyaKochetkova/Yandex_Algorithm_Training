73a4c0d607b0db1ec00d79aa4342d10a6e12a696
AsyaKochetkova <zinets.an@gmail.com>
2024-04-16 23:11:53 +0300

    problem was in type's sizes

--- warmup/4_0_3.cpp
+++ warmup/4_0_3.cpp
@@ -3,21 +3,26 @@
 #include <iomanip>
 
 int main(){
-    int ax,ay,bx,by;
-    double dist,ar,br,res;
+    long long int ax,ay,bx,by;
+    long double dist,ar,br,res;
 
     std::cin >> ax >> ay >> bx >> by;
-    ar = std::hypot(ax,ay);
-    br = std::hypot(bx,by);
+    ar = std::hypotl(ax,ay);
+    br = std::hypotl(bx,by);
+    
+
+    if(ar < br){int cx=ax,cy=ay,cr = ar;ax = bx;ay = by; bx = cx;by = cy; ar = br; br = cr; };
     //std::cout << ar << " " << br << "\n";
-
-    if(ar < br){int cx=ax,cy=ay;ax = bx;ay = by; bx = cx;by = cy;};
-
     //std::cout << std::acos((ax*bx+ay*by)/(ar*br)) << "\n";
-    res = std::acos((ax*bx+ay*by)/(ar*br))*br + ar-br;
-    if(ar+br < res || ar==0 || br == 0) res = ar+br;
+    
+    if(ax*by == ay*bx && (ax*bx+ay*by) > 0) {res = ar-br;} //std::cout << "1\n";}
+    else if(ax*by == ay*bx) {res = ar+br;}// std::cout << "2\n";}
+    else res = std::acos((ax*bx+ay*by)/(ar*br))*br + ar-br;
+    //std::cout << res << "\n";
+    if(ar+br < res || ar==0 || br == 0) {res = ar+br;};
 
-    std::cout << std::setprecision(8) << res;
+    std::cout << std::setprecision(8) << res << "\n";
+    //std::cout << std::setprecision(8) << res << "\n";
         
 
 
